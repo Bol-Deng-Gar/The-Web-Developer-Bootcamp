a6e09c40bb18a0b6c3f5e2b505843878f09bbd2b
Bol Deng Gar <boldenggar@gmail.com>
2024-07-21 13:44:55 +0200

    Made each player an object

--- pingpong.js
+++ pingpong.js
@@ -1,75 +1,80 @@
+const p1 = {
+  score: 0,
+  button: document.querySelector("#button-1"),
+  player: document.querySelector("#player-one")
+}
+
+const p2 = {
+  score: 0,
+  button: document.querySelector("#button-2"),
+  player: document.querySelector("#player-two")
+}
+
+const reset = {
+  button: document.querySelector("#button-3"),
+}
+
 const selectElement = document.querySelector("#number-select")
 let score = document.querySelector("#score")
 let amountOfRounds = parseInt(selectElement.value, 10);
-const playerOneButton = document.querySelector("#button-1")
-const playerTwoButton = document.querySelector("#button-2")
-const resetButton = document.querySelector("#button-3")
-const playerOne = document.querySelector("#player-one")
-const playerTwo = document.querySelector("#player-two")
-
-let playerOneScore = 0;
-let playerTwoScore = 0;
 
 selectElement.addEventListener("change", () => {
   amountOfRounds = parseInt(selectElement.value, 10);
 });
 
-playerOneButton.addEventListener("click", () => {
-  playerOneScore += 1
-  playerOne.textContent = playerOneScore
+function updateScores(player) {
+  player.score += 1
+  player.player.textContent = player.score
   checkForWinner();
-})
+}
 
-playerTwoButton.addEventListener("click", () => {
-  playerTwoScore += 1
-  playerTwo.textContent = playerTwoScore
-  checkForWinner();
-})
+p1.button.addEventListener("click", () => updateScores(p1, p2));
+p2.button.addEventListener("click", () => updateScores(p2, p1));
 
-resetButton.addEventListener("click", () => {
-  playerOneScore = 0;
-  playerTwoScore = 0;
-  playerOne.textContent = playerOneScore
-  playerTwo.textContent = playerTwoScore
-  playerOne.style.color = "black"
-  playerTwo.style.color = "black"
-  playerOneButton.style.color = "white";
-  playerTwoButton.style.color = "white";
-  playerOneButton.classList.remove('disabled-button');
-  playerTwoButton.classList.remove('disabled-button');
+reset.button.addEventListener("click", () => {
+  p1.score = 0;
+  p2.score = 0;
+  p1.player.textContent = p1.score
+  p2.player.textContent = p2.score
+  p1.player.style.color = "black"
+  p2.player.style.color = "black"
+  p1.button.style.color = "white";
+  p2.button.style.color = "white";
+  p1.button.classList.remove('disabled-button');
+  p2.button.classList.remove('disabled-button');
 })
 
 function checkForWinner() {
-  if (playerOneScore === amountOfRounds) {
-    playerOneButton.style.color = "white";
-    playerTwoButton.style.color = "white";
-    playerOneWins();
-  } else if (playerTwoScore === amountOfRounds) {
-    playerOneButton.style.color = "white";
-    playerTwoButton.style.color = "white";
-    playerTwoWins();
+  if (p1.score === amountOfRounds) {
+    p1.button.style.color = "white";
+    p2.button.style.color = "white";
+    p1Wins();
+  } else if (p2.score === amountOfRounds) {
+    p1.button.style.color = "white";
+    p2.button.style.color = "white";
+    p2Wins();
   }
 }
 
 
-function playerOneWins() {
-  playerOne.style.color = "green"
-  playerTwo.style.color = "red"
-  playerOneButton.style.color = "rgba(67, 232, 130, 0.860)"
-  playerTwoButton.style.color = "rgba(59, 85, 255, 0.860)"
-  playerOneButton.classList.add('disabled-button');
-  playerTwoButton.classList.add('disabled-button');
-  playerOneButton.style.color = "white";
-  playerTwoButton.style.color = "white";
+function p1Wins() {
+  p1.player.style.color = "green"
+  p2.player.style.color = "red"
+  p1.button.style.color = "rgba(67, 232, 130, 0.860)"
+  p2.button.style.color = "rgba(59, 85, 255, 0.860)"
+  p1.button.classList.add('disabled-button');
+  p2.button.classList.add('disabled-button');
+  p1.button.style.color = "white";
+  p2.button.style.color = "white";
 }
 
-function playerTwoWins() {
-  playerOne.style.color = "red"
-  playerTwo.style.color = "green"
-  playerOneButton.style.backgroundColor = "rgba(67, 232, 130, 0.860)"
-  playerTwoButton.style.backgroundColor = "rgba(59, 85, 255, 0.860)"
-  playerOneButton.classList.add('disabled-button');
-  playerTwoButton.classList.add('disabled-button');
-  playerOneButton.style.color = "white";
-  playerTwoButton.style.color = "white";
+function p2Wins() {
+  p1.player.style.color = "red"
+  p2.player.style.color = "green"
+  p1.button.style.backgroundColor = "rgba(67, 232, 130, 0.860)"
+  p2.button.style.backgroundColor = "rgba(59, 85, 255, 0.860)"
+  p1.button.classList.add('disabled-button');
+  p2.button.classList.add('disabled-button');
+  p1.button.style.color = "white";
+  p2.button.style.color = "white";
 }
