6e81005a7e91f0cae01a08bda63aadafaefa69b8
Bol Deng Gar <boldenggar@gmail.com>
2024-07-25 08:59:45 +0200

    Implement fakeRequest function with promises for async simulation

---
+++ promises.js
@@ -0,0 +1,20 @@
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const rand = Math.random();
+    setTimeout(() => {
+      if (rand < 0.7) {
+        resolve("Your fake data here");
+      }
+      reject("Request Error!");
+    }, 1000)
+  })
+}
+
+fakeRequest("/dogs/1")
+  .then((data) => {
+    console.log("DONE WITH REQUEST!")
+    console.log("Data is:", data)
+  })
+  .catch((err) => {
+    console.log("Something went wrong!")
+  })
